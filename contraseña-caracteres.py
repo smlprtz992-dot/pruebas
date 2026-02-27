7fd42b0fad44d4057a4ae8ca197ff39706f9728b
Samuel <263939895+smlprtz992-dot@users.noreply.github.com>
2026-02-27 09:53:54 -0500

    initial commit

---
+++ contraseña-caracteres.py
@@ -0,0 +1,23 @@
+contraseña_almacenada = ("12345")
+
+contraseña_digitada = input("ingrese una contraseña de 5 digitos: ")
+
+array1 = list (contraseña_almacenada)
+array2 = list (contraseña_digitada)
+
+len(array1) == len(array2)
+
+cont = 0
+
+for i in range(len(array1)):
+    print("comparando el digito", array1[i] , "con el digito" , array2[i])
+    if array1[i] == array2[i]:
+        cont += 1
+        print("el digito" , array1[i] , "es correcto")
+        print("la cantidad de digitos correctos es:" , cont)
+
+
+if cont == len(array1) :
+    print("contraseña correcta, acceso concedido")
+else :
+    print("contraseña correcta acceso denegado")        
---
+++ saldo.py
@@ -0,0 +1,55 @@
+saldo = float(input("ingrese su saldo:"))
+op = 0
+
+while op != "4":
+    print("\n---Menu---")
+    print("1. consultar saldo")
+    print("2. deposito")
+    print("3. retiro")
+    print("4. salir")
+
+    op = input("elige una opcion:")
+
+    if op == "1":
+        print("su saldo es:" , saldo)
+
+
+    elif op == "2":
+        deposito = float(input("Ingrese monto a depositar: "))
+        if deposito > 0:
+            saldo += deposito
+            print(f"Deposito exitoso. Tu saldo es {saldo}")
+        else:
+            print(f"Monto invalido.")
+        
+
+    elif op == "3":
+        retiro = float(input("Ingrese valor a retirar: ")) 
+        if retiro > saldo:
+            print("Saldo insuficiente")
+        elif retiro <= 0:
+            print("Monto de retiro invalido")
+        else:
+            saldo -= retiro
+            print(f"Retiro exitoso. Nuevo saldo {saldo}")
+
+
+    elif op == "4":
+        print("Gracias por usar nuestro sistema")
+    else:
+        print("Opcion invalida")
+
+
+
+
+             
+                           
+
+
+
+ 
+ 
+ 
+
+
+
